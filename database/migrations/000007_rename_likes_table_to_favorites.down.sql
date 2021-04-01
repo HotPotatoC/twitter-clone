4db4370b3fa9f2f5ad2a4f3a5f06687ba6062ded
HotPotatoC <juandotulung@gmail.com>
2021-04-01 19:08:19 +0700

    refactor: renamed likes table to favorites

---
+++ database/migrations/000007_rename_likes_table_to_favorites.down.sql
@@ -0,0 +1,6 @@
+BEGIN;
+ALTER TABLE IF EXISTS favorites DROP CONSTRAINT IF EXISTS favorites_id_tweet_id_user_unique;
+ALTER TABLE IF EXISTS favorites DROP CONSTRAINT IF EXISTS favorites_id_tweet_foreign;
+ALTER TABLE IF EXISTS favorites DROP CONSTRAINT IF EXISTS favorites_id_user_foreign;
+DROP TABLE IF EXISTS favorites;
+COMMIT;
