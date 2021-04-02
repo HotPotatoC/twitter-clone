1a870987e77a5996cc86e5fed289bd0c42eecf8f
HotPotatoC <juandotulung@gmail.com>
2021-04-02 17:32:47 +0700

    feat: added more information on list tweets services

--- internal/module/tweet/service/list_tweet_service.go
+++ internal/module/tweet/service/list_tweet_service.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"database/sql"
 	"time"
 
 	"github.com/HotPotatoC/twitter-clone/internal/module/tweet/entity"
@@ -10,7 +11,11 @@ import (
 
 type ListTweetOutput struct {
 	entity.Tweet
-	Name string `json:"name"`
+	Name           string `json:"name"`
+	RepliedToTweet int64  `json:"replied_to_tweet_id,omitempty"`
+	RepliedToName  string `json:"replied_to_name,omitempty"`
+	FavoritesCount int    `json:"favorites_count"`
+	RepliesCount   int    `json:"replies_count"`
 }
 
 type ListTweetService interface {
@@ -30,23 +35,41 @@ func (s listTweetService) Execute() ([]ListTweetOutput, error) {
 	rows, err := s.db.Query(`
 	SELECT tweets.id,
 		tweets.content,
-		tweets.id_user,
 		tweets.created_at,
-		users.name
+		(ARRAY_AGG(users.name))[1],
+		(ARRAY_AGG(sq.id_tweet))[1],
+		(ARRAY_AGG(sq.name))[1],
+		COUNT(f.*),
+		COUNT(r1.*)
 	FROM tweets
-    INNER JOIN users ON tweets.id_user = users.id
-	`)
+		LEFT JOIN users ON users.id = tweets.id_user
+		LEFT JOIN (
+			SELECT replies.id_reply,
+				replies.id_tweet,
+				users.name
+			FROM replies
+				INNER JOIN tweets as t ON t.id = replies.id_tweet
+				INNER JOIN users ON users.id = t.id_user
+			WHERE replies.id_tweet = t.id
+		) as sq ON sq.id_reply = tweets.id
+		LEFT JOIN favorites as f ON f.id_tweet = tweets.id
+		LEFT JOIN replies as r1 ON r1.id_tweet = tweets.id
+	GROUP BY tweets.id
+	ORDER BY tweets.created_at DESC`)
 	if err != nil {
 		return []ListTweetOutput{}, errors.Wrap(err, "service.listTweetService.Execute")
 	}
 	defer rows.Close()
 
 	for rows.Next() {
-		var id, userID int64
+		var id int64
 		var content, name string
+		var repliedToTweetID sql.NullInt64
+		var repliedToName sql.NullString
 		var createdAt time.Time
+		var favoritesCount, repliesCount int
 
-		err = rows.Scan(&id, &content, &userID, &createdAt, &name)
+		err = rows.Scan(&id, &content, &createdAt, &name, &repliedToTweetID, &repliedToName, &favoritesCount, &repliesCount)
 		if err != nil {
 			return []ListTweetOutput{}, errors.Wrap(err, "service.listTweetService.Execute")
 		}
@@ -57,7 +80,11 @@ func (s listTweetService) Execute() ([]ListTweetOutput, error) {
 				Content:   content,
 				CreatedAt: createdAt,
 			},
-			Name: name,
+			Name:           name,
+			RepliedToTweet: repliedToTweetID.Int64,
+			RepliedToName:  repliedToName.String,
+			FavoritesCount: favoritesCount,
+			RepliesCount:   repliesCount,
 		})
 	}
 
