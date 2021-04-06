d735509c8da5ea63d499ad55f51b16ef89d7c31a
HotPotatoC <juandotulung@gmail.com>
2021-04-06 17:03:40 +0700

    feat: added more fields in get user service

--- internal/module/user/service/get_user_service.go
+++ internal/module/user/service/get_user_service.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"database/sql"
 	"time"
 
 	"github.com/HotPotatoC/twitter-clone/internal/module/user/entity"
@@ -11,6 +12,10 @@ import (
 type GetUserOutput struct {
 	ID              int64     `json:"id"`
 	Name            string    `json:"name"`
+	Bio             string    `json:"bio"`
+	Location        string    `json:"location"`
+	Website         string    `json:"website"`
+	BirthDate       time.Time `json:"birth_date"`
 	FollowersCount  int       `json:"followers_count"`
 	FollowingsCount int       `json:"followings_count"`
 	JoinedAt        time.Time `json:"joined_at"`
@@ -41,11 +46,17 @@ func (s getUserService) Execute(userID int64) (GetUserOutput, error) {
 
 	var id int64
 	var name string
+	var bio, location, website sql.NullString
+	var birthDate sql.NullTime
 	var joinedAt time.Time
 	var followingsCount, followersCount int
 	err = s.db.QueryRow(`
 	SELECT u.id,
 		u.name,
+		u.bio,
+		u.location,
+		u.website,
+		u.birth_date,
 		u.created_at,
 		COUNT(f1.*) AS followings_count,
 		COUNT(f2.*) AS followers_count
@@ -54,7 +65,7 @@ func (s getUserService) Execute(userID int64) (GetUserOutput, error) {
 		LEFT JOIN follows AS f2 ON f2.followed_id = u.id
 	WHERE u.id = $1
 	GROUP BY u.id
-	`, userID).Scan(&id, &name, &joinedAt, &followingsCount, &followersCount)
+	`, userID).Scan(&id, &name, &bio, &location, &website, &birthDate, &joinedAt, &followingsCount, &followersCount)
 	if err != nil {
 		return GetUserOutput{}, errors.Wrap(err, "service.getUserService.Execute")
 	}
@@ -62,6 +73,10 @@ func (s getUserService) Execute(userID int64) (GetUserOutput, error) {
 	return GetUserOutput{
 		ID:              id,
 		Name:            name,
+		Bio:             bio.String,
+		Location:        location.String,
+		Website:         website.String,
+		BirthDate:       birthDate.Time,
 		FollowersCount:  followersCount,
 		FollowingsCount: followingsCount,
 		JoinedAt:        joinedAt,
