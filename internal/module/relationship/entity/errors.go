bcacca51babe589bf550c49b1b31fdaf5d8b463c
HotPotatoC <juandotulung@gmail.com>
2021-04-19 21:19:50 +0700

    style: fmt vet

--- internal/module/relationship/entity/errors.go
+++ internal/module/relationship/entity/errors.go
@@ -4,5 +4,5 @@ import "errors"
 
 var (
 	ErrUserAlreadyFollowed = errors.New("user already followed")
-	ErrUserIsNotFollowing = errors.New("user is not following")
+	ErrUserIsNotFollowing  = errors.New("user is not following")
 )
--- internal/module/tweet/entity/errors.go
+++ internal/module/tweet/entity/errors.go
@@ -3,6 +3,6 @@ package entity
 import "errors"
 
 var (
-	ErrTweetDoesNotExist = errors.New("tweet does not exist")
+	ErrTweetDoesNotExist     = errors.New("tweet does not exist")
 	ErrTweetAlreadyFavorited = errors.New("tweet already favorited")
 )
