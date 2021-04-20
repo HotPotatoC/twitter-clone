85faef0f86bef3a54b07557c6123b465e5a0fab3
HotPotatoC <juandotulung@gmail.com>
2021-04-20 14:53:56 +0700

    style: moved App component into main.ts

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,12 +1,40 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import makeFontAwesomePlugin from './plugins/font-awesome'
+import { createApp, defineComponent, resolveComponent, h } from 'vue'
+import { useRouter } from 'vue-router'
 import { router } from './routes'
-import { store } from './store'
+import { store, useStore } from './store'
+import axios from './services/axios'
+import { ActionTypes } from './modules/auth/store/actions'
+import makeFontAwesomePlugin from './plugins/font-awesome'
 
 import './assets/styles/root.css'
 
-const app = createApp(App)
+const app = createApp(
+  defineComponent({
+    setup() {
+      const store = useStore()
+      const router = useRouter()
+      const RouterView = resolveComponent('router-view')
+
+      const responseInterceptor = axios.interceptors.response.use(
+        (res) => res,
+        async (error) => {
+          if (error.response.status === 401) {
+            axios.interceptors.response.eject(responseInterceptor)
+
+            await store.dispatch(ActionTypes.REFRESH_AUTH_TOKEN)
+            await store.dispatch(ActionTypes.GET_USER_DATA)
+            if (!store.getters['isLoggedIn']) {
+              router.push('/login')
+              return Promise.reject(error)
+            }
+          }
+        }
+      )
+
+      return () => h(RouterView)
+    },
+  })
+)
 
 app.component('FontAwesome', makeFontAwesomePlugin())
 
