4b496b3849f5b6899f04125318c1580e3c2c1eaf
HotPotatoC <juandotulung@gmail.com>
2021-04-27 21:54:31 +0700

    style: fmt vet

--- internal/module/user/service/errors.go
+++ internal/module/user/service/errors.go
@@ -6,5 +6,5 @@ var (
 	ErrInvalidCursor = errors.New("invalid cursor")
 
 	ErrUploadImageSizeTooLarge = errors.New("upload image size too large")
-	ErrInvalidImageType = errors.New("invalid image type")
+	ErrInvalidImageType        = errors.New("invalid image type")
 )
